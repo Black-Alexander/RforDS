0df4caf9abdc71c2bb108460590cd12d5186d409
David Ly <davidtnly@gmail.com>
2018-04-15 23:46:53 -0700

    ended at 5.2.4 exercises

--- chapter5-8.R
+++ chapter5-8.R
@@ -1,6 +1,108 @@
 ## setwd('C:/Users/Sky/Documents/R/win-library/3.4')
 
 library(tidyverse)
+library(nycflights13)
 
 ####### Chapter 5: Data Transformation
+
+flights
+
+## print in tibble if you want to view everything
+## tibbles are df but tweaked to work better in tidyverse
+
+## key dplyr functions: filter(), arrange(), select(), mutate(), summarise()
+
+jan1 <- filter(flights, month == 1, day == 1)
+(dec25 <- filter(flights, month == 12, day == 25)) ## Show data with extra ()
+
+(nov_dec <- filter(flights, month %in% c(11, 12)))
+
+## filter() over includes rows where condition = TRUE
+
+df <- tibble(x = c(1, NA, 3))
+df1 <- data.frame(x = c(1, NA, 3))
+
+filter(df, x > 1)
+filter(df, is.na(x) | x > 1)
+
+## 5.2.4 Exercises
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
