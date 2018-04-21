c7fb2cdbb61f277fba3e8819fc4d74331528c355
David Ly <davidtnly@gmail.com>
2018-04-21 13:42:42 -0700

    finished 7.4

--- chapter5-8.R
+++ chapter5-8.R
@@ -174,27 +174,74 @@ popular_dests %>%
   arrange(dest)
 
 
+####### Chapter 7: Exploratory Data Analysis
+
+# Generate questions about your data
+# Search for answers by visualizing, transforming, and modelling
+# use what you learn to refine your questions and/or generate new Q's
+
+## Visualizing distritibutions
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(cut))
+
+diamonds %>% 
+  count(cut)
+
+ggplot(data = diamonds) +
+  geom_histogram(mapping = aes(x = carat), binwidth = 0.5)
+
+## Find counts from the histogram
+
+diamonds %>% 
+  count(cut_width(carat, 0.5))
+
+## Overlay multiple histograms then use geom_freqpoly
+
+smaller <- diamonds %>% 
+  filter(carat < 3)
+
+ggplot(data = smaller, mapping = aes(x = carat, colour = cut)) +
+  geom_freqpoly(binwidth = 0.1)
+
+ggplot(diamonds) + 
+  geom_histogram(mapping = aes(x = y), binwidth = 0.5) +
+  coord_cartesian(ylim = c(0, 50))
+
+(unusual <- diamonds %>% 
+  filter(y < 3 | y > 20) %>% 
+  select(price, x, y, z) %>%
+  arrange(y)
+)
+
+## Replace missing values so data is not included using mutate()
+
+diamonds2 <- diamonds %>%
+  mutate(y = ifelse(y < 3 | y > 20, NA, y))
+
+head(diamonds2$y)
+
+ggplot(data = diamonds2, mapping = aes(x = x, y = y)) + 
+  geom_point()
+
+## Suppress warning na warning with na.rm = TRUE
+
+ggplot(data = diamonds2, mapping = aes(x = x, y = y)) + 
+  geom_point(na.rm = TRUE)
+
+## Use mutate() to create new variables for the df
+
+nycflights13::flights %>% 
+  mutate(
+    cancelled = is.na(dep_time),
+    sched_hour = sched_dep_time %/% 100,
+    sched_min = sched_dep_time %% 100,
+    sched_dep_time = sched_hour + sched_min / 60
+  ) %>% 
+  ggplot(mapping = aes(sched_dep_time)) + 
+  geom_freqpoly(mapping = aes(colour = cancelled), binwidth = 1/4)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
