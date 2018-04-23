a8ec981ce53e2a38b9c88669957a882ae3d4f282
David Ly <davidtnly@gmail.com>
2018-04-22 23:06:31 -0700

    scrolled through chapter 12 - will work on exercises to study gather/spread and started new r-code for chapter 13

--- chapter9-12.R
+++ chapter9-12.R
@@ -188,48 +188,22 @@ parse_date("01/02/15", "%y/%m/%d")
 
 ####### Chapter 12: Tidy Data
 
+tidy4a <- table4a %>% 
+  gather(`1999`, `2000`, key = "year", value = "cases")
+
+tidy4b <- table4b %>% 
+  gather(`1999`, `2000`, key = "year", value = "population")
+
+left_join(tidy4a, tidy4b)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+stocks <- tibble(
+  year   = c(2015, 2015, 2016, 2016),
+  half  = c(   1,    2,     1,    2),
+  return = c(1.88, 0.59, 0.92, 0.17)
+)
+stocks %>% 
+  spread(year, return) %>% 
+  gather("year", "return", `2015`:`2016`)
 
 
