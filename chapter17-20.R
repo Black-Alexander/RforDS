0154561bc369e3628b45a1c3b54a239b872e58ab
David <davidtnly@gmail.com>
2018-08-09 17:40:36 -0700

    created new script for new chapters, will work on chapter 14-16 after

---
+++ chapter17-20.R
@@ -0,0 +1,22 @@
+library(purrr)
+
+## Reducing code duplication has 3 main benefits
+# 1. It's easier to see the intent of the code
+# 2. It's easier to response to changes in requirements. It's easier to make changes in one place than several.
+# 3. You're likely to have fewer bugs because each line of code is used in more places.
+
+## Tools to Reduce
+# One tool for reducing duplication is functions, which reduce duplication by identifying same patterns.
+# Another tool to reduce duplication is iteration.
+
+## This chapter will teach you about two important iteration paradigms:
+# 1. Imperative Programming
+  # Tools like for loops and while loops
+# 2. Functional Programming
+  # Tools to extract out this duplicated code, so for each common for loop pattern gets its own function
+
+#**************
+## Master vocabulary for FP, you can solve many common iteration problems with less code, more ease, and fewer errors.
+#**************
+
+
